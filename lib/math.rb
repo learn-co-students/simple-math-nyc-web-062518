7bce0fe7af7647b2fd795a816a39fb40284d09df
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-22 01:15:53 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
 def addition(num1, num2)
+  answer=num1.to_f+num2.to_f
 end
 
 def subtraction(num1, num2)
+  answer=num1.to_f-num2.to_f
 end
 
 def division(num1, num2)
+  answer=num1.to_f/num2.to_f
 end
 
 def multiplication(num1, num2)
+  answer=num1.to_f*num2.to_f
 end
 
 def modulo(num1, num2)
+  answer=num1.to_f%num2.to_f
 end
 
 def square_root(num)
+  answer=Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  answer=  num1 + (( num2 * num3 ) / num4 )
 end
